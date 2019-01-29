69899e13a6e709ba1f0fb625bd5428160882475a
Peter <peter_brv@yahoo.com>
2019-01-29 09:01:07 -0800

    Creating buffer

--- JustifyText.py
+++ JustifyText.py
@@ -1,9 +1,13 @@
 f = open('sample.txt', 'r')
 paragraph = f.read()
 #print (paragraph.split())
-
-print(paragraph.split())
-
-for i in paragraph.split():
-    print(i)
-
+words = paragraph.split()
+# need buffer to store words
+buffer = []
+#looping through all words
+for x in range(len(words)):
+    word = words[x]
+    # creating a condition for buffer
+    if sum([len(i) for i in buffer]) + len(word) + len(buffer) <= 20:
+        buffer.append(word)
+        flag = False
